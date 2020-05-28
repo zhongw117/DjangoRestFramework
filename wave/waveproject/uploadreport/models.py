8da96dd2b9a4479fe39e53e4ef32f24b79a678fb
Wilson <wilson@Wilsons-MacBook-Pro-2.local>
2020-05-28 14:17:43 -0400

    second commit

--- wave/waveproject/uploadreport/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from datetime import datetime
-class Payroll(models.Model):
-    date = models.CharField(max_length=20, null=False)
-    hours_worked = models.FloatField()
-    employee_id = models.IntegerField(null=False)
-    job_group = models.CharField(max_length=1)
-    def __str__(self):
-        return self.date
--- wave/waveproject/waveproject/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from uploadreport.views import csv_upload
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('csv-upload/', csv_upload, name="profile_upload"),
-]
